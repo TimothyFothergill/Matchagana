4a10648cc3c0a7355556b46cb51762dc06829495
TimothyFothergill <timothy.fothergill@digital.hmrc.gov.uk>
2022-04-24 12:18:03 +0100

    CVG-1036 adds katakana game mode

--- app/models.py
+++ app/models.py
@@ -1,4 +1,5 @@
 from    dataclasses import dataclass
+from    enum        import Enum
 import  unicodedata
 
 @dataclass
@@ -16,17 +17,27 @@ class Matchagana:
                             str(romaji).upper()
                         )
 
-    def small(self):
-        char = "HIRAGANA LETTER SMALL " + str(self.romaji.upper())
+    def small(self, kana):
+        match kana:
+            case KanaMode.HIRAGANA:         
+                char = "HIRAGANA LETTER SMALL " + str(self.romaji.upper())
+            case KanaMode.KATAKANA:
+                char = "KATAKANA LETTER SMALL " + str(self.romaji.upper())
         return unicodedata.lookup(char)
 
 @dataclass
 class CurrentRound:
-    score                               : int           = 0
-    romaji_one                          : str           = ""
-    romaji_two                          : str           = ""
-    romaji_three                        : str           = ""
-    next_hiragana                       : str           = ""
-    match                               : int           = 0
-    fail                                : int           = 0
-    correct_hiragana_object             : Matchagana    = Matchagana("a")
+    score                      : int           = 0
+    romaji_one                 : str           = ""
+    romaji_two                 : str           = ""
+    romaji_three               : str           = ""
+    next_kana                  : str           = ""
+    match                      : int           = 0
+    fail                       : int           = 0
+    correct_object             : Matchagana    = Matchagana("a")
+    game_mode                  : str           = ""
+
+class KanaMode(Enum):
+    MATCHAGANA  = 0
+    HIRAGANA    = 1
+    KATAKANA    = 2
